831a50257be7b918bb2ce867e593f843b3770c1a
abhi12-hue <abhi12bisht@gmail.com>
2025-03-31 20:23:26 +0530

    main

--- server/controller/PaymentController.js
+++ server/controller/PaymentController.js
@@ -60,7 +60,7 @@ exports.createCheckoutSession = async (req, res) => {
       },
       order_meta: {
         return_url: `https://vidhayala-ai-18.onrender.com/course-progress/${courseId}?order_id=${orderId}`,
-        notify_url: "https://vidhayala-ai-18.onrender.com", // Replace with your public URL
+        notify_url: "https://vidhayala-ai-18.onrender.com/webhook/cashfree"
       },
     };
 
